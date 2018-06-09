2bdb3a749b25a9f2d8ac935892568041f3547653
万鼎铭 <dingming.wan@sinobbd.com>
2018-06-09 21:03:13 +0800

    add weave

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-images=(kube-proxy-amd64:v1.7.2 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.7.2 kube-controller-manager-amd64:v1.7.2 kube-apiserver-amd64:v1.7.2 etcd-amd64:3.0.17 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.6.1)
+images=(kube-proxy-amd64:v1.7.2 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.7.2 kube-controller-manager-amd64:v1.7.2 kube-apiserver-amd64:v1.7.2 etcd-amd64:3.0.17 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.6.1 weave-kube:2.3.0)
 for imageName in ${images[@]} ; do
   docker pull caucyming/$imageName
   docker tag caucyming/$imageName gcr.io/google_containers/$imageName
